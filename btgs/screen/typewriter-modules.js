df33934b6cbd1048511346fcb1d9d569de324541
James <james@wallofhistory.com>
2022-07-31 18:10:20 -0500

    Update typewriter-modules.js

--- btgs/screen/typewriter-modules.js
+++ btgs/screen/typewriter-modules.js
@@ -13,6 +13,7 @@ xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
         if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
             questions = xhr.responseText.split("\r\n");
+            console.log(questions);
         }
     }
 }
