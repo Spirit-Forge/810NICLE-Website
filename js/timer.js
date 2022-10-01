7141a2e94fa0446a800248b8c2959d9dfe72aff6
James Browning <46514697+JSLBrowning@users.noreply.github.com>
2022-10-01 12:17:24 -0500

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -27,6 +27,6 @@ let x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "BEWARE.";
+    document.getElementById("countdown").innerHTML = "BEWARE.";
   }
 }, 1000);
