0012e6773b7d49b4f850e662b3b97b269c258fae
Vahkiti <36716882+Vahkiti@users.noreply.github.com>
2024-01-16 13:47:32 -0500

    Update zoom.js
    Added new trailer

--- js/zoom.js
+++ js/zoom.js
@@ -42,7 +42,7 @@ function addZoomEventListeners() {
             zoom.classList.add("zoom");
 
             document.body.appendChild(zoom);
-            zoom.innerHTML += '<iframe width="560" height="315" src="https://www.youtube.com/embed/-7vay6RzRu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            zoom.innerHTML += '<iframe width="560" height="315" src="https://www.youtube.com/embed/1dRfg_r6oOg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
             let exit = document.createElement("span");
             exit.classList.add("exitSpan");
